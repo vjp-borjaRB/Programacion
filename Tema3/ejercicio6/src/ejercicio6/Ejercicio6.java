54bd2543f02a99e899280fe2820bf1dda37bc23f
borr-13 <borr1316@gmail.com>
2025-10-07 05:47:05 +0200

    Update Ejercicio6.java

--- Tema3/ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Tema3/ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -13,13 +13,13 @@ public class Ejercicio6 {
         int nota;
         System.out.println("Introduce la nota del alumno:");
         nota = entrada.nextInt();
-        if (nota >= 0 && nota < 4) {
+        if (nota >= 0 && nota <= 4) {
             System.out.println("Suspenso");
-        } else if (nota > 5 && nota < 6) {
+        } else if (nota >= 5 && nota <= 6) {
             System.out.println("Bien");
-        } else if (nota > 7 && nota < 8) {
+        } else if (nota >= 7 && nota <= 8) {
             System.out.println("Notable");
-        } else if (nota > 9 && nota <= 10) {
+        } else if (nota >= 9 && nota <= 10) {
             System.out.println("Sobresaliente");
         } else if (nota < 0 || nota > 10) {
             System.err.println("Introduce una nota entre 0 y 10");
