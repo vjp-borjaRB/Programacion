725ec93a7b68f6b2674aec89d1014cb483e8716c
Neo <borr1316@gmail.com>
2025-10-30 17:10:21 +0100

    .

--- Tema4/ejercicio17/src/ejercicio17/Ejercicio17.java
+++ Tema4/ejercicio17/src/ejercicio17/Ejercicio17.java
@@ -1,5 +1,7 @@
 package ejercicio17;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Borja Romero
@@ -7,7 +9,27 @@ package ejercicio17;
 public class Ejercicio17 {
 
     public static void main(String[] args) {
+        generarLetra();
+
+    }
+
+    public static void generarLetra() {
+        double aleatorio;
+        int aleatorioEntero;
+        char letra;
+        aleatorio = Math.floor((Math.random() * ((122 - 97) + 1) + 97));
+        aleatorioEntero = (int) aleatorio;
+        letra = (char) aleatorioEntero;
+        System.out.println(letra);
         
     }
-    
+
+    public static char pedirLetra() {
+        Scanner entrada = new Scanner(System.in);
+        char letra;
+        System.out.println("Por favor, introduce una letra: ");
+        letra = entrada.nextLine().charAt(0);
+        return letra;
+    }
+
 }
