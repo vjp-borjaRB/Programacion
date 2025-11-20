a4561489841340c800db69c515b14f10b0185bad
borr-13 <borr1316@gmail.com>
2025-11-20 06:47:43 +0100

    .

---
+++ Tema5/ejercicio17/src/ejercicio17/Ejercicio17.java
@@ -0,0 +1,50 @@
+package ejercicio17;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ */
+public class Ejercicio17 {
+
+    public static void main(String[] args) {
+        Enunciado enunciado1 = new Enunciado("En que continente se encuentra Egipto?", "Africa");
+        Enunciado enunciado2 = new Enunciado("Cual es el rio mas largo del mundo?", "Nilo");
+        Enunciado enunciado3 = new Enunciado("Que planeta es conocido como el planeta rojo?", "Marte");
+        Enunciado enunciado4 = new Enunciado("Cual es el pais mas grande del mundo en superficie?", "Rusia");
+        System.out.println("Tu nota es: " + test(enunciado1, enunciado2, enunciado3, enunciado4));
+    }
+
+    public static String entradaUsuario() {
+        Scanner entrada = new Scanner(System.in);
+        String entradaUsuario = entrada.nextLine();
+        return entradaUsuario;
+    }
+
+    public static double test(Enunciado enunciado1, Enunciado enunciado2, Enunciado enunciado3, Enunciado enunciado4) {
+        double resultado1 = 0;
+        double resultado2 = 0;
+        double resultado3 = 0;
+        double resultado4 = 0;
+        double nota;
+        System.out.println(enunciado1.getPregunta());
+        if (enunciado1.getRespuesta().equalsIgnoreCase(entradaUsuario())) {
+            resultado1 = 2.5;
+        }
+        System.out.println(enunciado2.getPregunta());
+        if (enunciado2.getRespuesta().equalsIgnoreCase(entradaUsuario())) {
+            resultado2 = 2.5;
+        }
+        System.out.println(enunciado3.getPregunta());
+        if (enunciado3.getRespuesta().equalsIgnoreCase(entradaUsuario())) {
+            resultado3 = 2.5;
+        }
+        System.out.println(enunciado4.getPregunta());
+        if (enunciado4.getRespuesta().equalsIgnoreCase(entradaUsuario())) {
+            resultado4 = 2.5;
+        }
+        nota = resultado1 + resultado2 + resultado3 + resultado4;
+        return nota;
+    }
+}
