a707e4b07e1bc535683f851a4a943bebf4ec18c1
borr-13 <borr1316@gmail.com>
2025-11-22 06:02:36 +0100

    .

--- Tema5/ejercicio16/src/ejercicio16/Ejercicio16.java
+++ Tema5/ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 public class Ejercicio16 {
 
     public static void main(String[] args) {
-        Alumno alumno1 = new Alumno();
-        Alumno alumno2 = new Alumno();
-        Alumno alumno3 = new Alumno();
+        Alumno alumno1 = new Alumno(pedirNombre(), comprobarNota(pedirNota()));
+        Alumno alumno2 = new Alumno(pedirNombre(), comprobarNota(pedirNota()));
+        Alumno alumno3 = new Alumno(pedirNombre(), comprobarNota(pedirNota()));
     }
 
     public static String pedirNombre() {
@@ -25,10 +25,29 @@ public class Ejercicio16 {
         double nota = entrada.nextDouble();
         return nota;
     }
-    
-    public static boolean comprobarNota(double nota) {
-    
+
+    public static double comprobarNota(double nota) {
+        boolean notaUsuario = false;
+        if (nota >= 0 && nota <= 10) {
+            notaUsuario = true;
+        }
+        while (!notaUsuario) {
+            System.out.println("Introduce una nota valida: ");
+            nota = pedirNota();
+        }
+        return nota;
     }
 
-        
+    public static void mostrarCalificacion(double nota) {
+        if (nota >= 0 && nota <= 4) {
+            System.out.println("Suspenso");
+        } else if (nota >= 5 && nota <= 6) {
+            System.out.println("Bien");
+        } else if (nota >= 7 && nota <= 8) {
+            System.out.println("Notable");
+        } else {
+            System.out.println("Sobresaliente");
+        }
+    }
+
 }
