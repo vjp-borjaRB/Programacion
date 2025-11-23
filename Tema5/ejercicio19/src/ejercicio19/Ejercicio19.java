caaab1ea064d065940ef580ef6ec2631dbcbbc55
borr-13 <borr1316@gmail.com>
2025-11-23 05:12:01 +0100

    Entregables

--- Tema5/ejercicio19/src/ejercicio19/Ejercicio19.java
+++ Tema5/ejercicio19/src/ejercicio19/Ejercicio19.java
@@ -8,8 +8,8 @@ public class Ejercicio19 {
 
     public static void main(String[] args) {
         
-        Empleado empleado1 = new Empleado("manolo", 40, 15);
-        Empleado empleado2 = new Empleado("Pepe", 50, 18);
+        Empleado empleado1 = new Empleado("David", 40, 15);
+        Empleado empleado2 = new Empleado("Jose", 50, 18);
         Empleado empleado3 = new Empleado("Fernando", 47, 13);
         empleado1.calcularSalario();
         empleado2.calcularSalario();
