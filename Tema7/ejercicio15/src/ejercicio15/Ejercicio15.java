9c0246c3fdedf2a7e1b716303f8473e2b43eb743
Neo <borr1316@gmail.com>
2026-01-23 20:38:23 +0100

    .

---
+++ Tema7/ejercicio15/src/ejercicio15/Ejercicio15.java
@@ -0,0 +1,116 @@
+package ejercicio15;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Borja Romero
+ */
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        int[] ventasCoches = new int[12];
+        String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        menu(ventasCoches, meses);
+    }
+
+    public static void menu(int[] ventasCoches, String[] meses) {
+        int menu;
+        do {
+            mostrarMenu();
+            menu = pedirOpcion();
+            switch (menu) {
+                case 1 -> {
+                    rellenarArray(ventasCoches);
+                }
+                case 2 -> {
+                    mostrarVentas(ventasCoches);
+                }
+                case 3 -> {
+                    mostrarVentasReves(ventasCoches);
+                }
+                case 4 -> {
+                    mostrarTotalVentas(ventasCoches);
+                }
+                case 5 -> {
+                    ventasMesesPares(ventasCoches);
+                }
+                case 6 -> {
+                    mesMasVentas(ventasCoches, meses);
+                }
+                case 7 -> {
+                }
+            }
+        } while (menu != 7);
+    }
+
+    public static int pedirOpcion() {
+        Scanner entrada = new Scanner(System.in);
+        int opcion = entrada.nextInt();
+        return opcion;
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("1. Rellenar array");
+        System.out.println("2. Mostrar las ventas introducidas en el punto anterior.");
+        System.out.println("3. Mostrar las ventas introducidas al reves.");
+        System.out.println("4. Suma total de ventas del año.");
+        System.out.println("5. Ventas totales de los meses pares.");
+        System.out.println("6. Nombre del mes con más ventas.");
+        System.out.println("7. Salir del programa");
+    }
+
+    public static void rellenarArray(int[] ventasCoches) {
+        int nventas;
+        for (int i = 0; i < ventasCoches.length; i++) {
+            nventas = (int) (Math.random() * 91) + 10;
+            ventasCoches[i] = nventas;
+        }
+    }
+
+    public static void mostrarVentas(int[] ventasCoches) {
+        System.out.println("Mostrando ventas: ");
+        for (int i = 0; i < ventasCoches.length; i++) {
+            System.out.println(ventasCoches[i]);
+        }
+    }
+
+    public static void mostrarVentasReves(int[] ventasCoches) {
+        System.out.println("Mostrando ventas al reves: ");
+        for (int i = ventasCoches.length - 1; i >= 0; i--) {
+            System.out.println(ventasCoches[i]);
+        }
+    }
+
+    public static void mostrarTotalVentas(int[] ventasCoches) {
+        int totalVentas = 0;
+        for (int i = 0; i < ventasCoches.length; i++) {
+            totalVentas += ventasCoches[i];
+        }
+        System.out.println(totalVentas);
+    }
+
+    public static void ventasMesesPares(int[] ventasCoches) {
+        int totalVentasPares = 0;
+        for (int i = 0; i < ventasCoches.length; i++) {
+            if (i % 2 != 0) {
+                totalVentasPares += ventasCoches[i];
+            }
+        }
+        System.out.println("El total de ventas de los meses pares es: " + totalVentasPares);
+    }
+
+    public static void mesMasVentas(int[] ventasCoches, String[] meses) {
+        String mejorMes = "";
+        int maxVentasMes = 0;
+        for (int i = 0; i < ventasCoches.length; i++) {
+            if (ventasCoches[i] > maxVentasMes) {
+                maxVentasMes = ventasCoches[i];
+            }
+            if (ventasCoches[i] == maxVentasMes) {
+            
+            }
+            
+        }
+    }
+}
