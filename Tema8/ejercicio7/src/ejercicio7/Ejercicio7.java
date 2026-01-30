49dccd8617299203be5715284f0da138ba5bbe6c
vjp-borjaRB <borr1316@gmail.com>
2026-01-30 06:12:00 +0100

    .

--- Tema8/ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Tema8/ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -11,21 +11,27 @@ public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Dia[] dia = new Dia[30];
+        String[] nombreDias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+        menu(entrada, dia, nombreDias);
     }
 
-    public static void menu(Scanner entrada) {
+    public static void menu(Scanner entrada, Dia[] dia, String[] nombreDias) {
         int menu;
         do {
             mostrarMenu();
             menu = entrada.nextInt();
             switch (menu) {
                 case 1 -> {
+                    rellenarDatos(dia, nombreDias);
                 }
                 case 2 -> {
+                    mostrarTemperaturas(dia);
                 }
                 case 3 -> {
+                    mostrarTempmedia(dia);
                 }
                 case 4 -> {
+                    mostrarCalurosos(dia);
                 }
                 case 5 -> {
                 }
@@ -39,5 +45,47 @@ public class Ejercicio7 {
         System.out.println("3. Visualizar la temperatura media del mes");
         System.out.println("4. Dia o dias mas calurosos del mes");
         System.out.println("5. Salir del programa");
+    }
+
+    public static void rellenarDatos(Dia[] dia, String[] nombreDias) {
+        int diaInicio = (int) (Math.random() * 7);
+        int temperatura;
+        for (int i = 0; i < dia.length; i++) {
+            dia[i] = new Dia();
+            String nombreDia = nombreDias[(diaInicio + i) % 7]; // %7 para no salirse del array
+            dia[i].setNombreDia(nombreDia);
+            temperatura = (int) (Math.random() * 26) - 10;
+            dia[i].setTemperatura(temperatura);
+        }
+    }
+
+    public static void mostrarTemperaturas(Dia[] dia) {
+        for (int i = 0; i < dia.length; i++) {
+            System.out.println(dia[i].getNombreDia() + " dia " + i + ": " + dia[i].getTemperatura() + " ");
+        }
+    }
+
+    public static void mostrarTempmedia(Dia[] dia) {
+        int tempMedia = 0;
+        for (Dia i : dia) {
+            tempMedia += i.getTemperatura();
+        }
+        System.out.println("La temperatura media del mes es: " + tempMedia / dia.length + " grados");
+    }
+
+    public static void mostrarCalurosos(Dia[] dia) {
+        int tempmax = 0;
+        for (Dia i : dia) {
+            if (tempmax < i.getTemperatura()) {
+                tempmax = i.getTemperatura();
+            }
+        }
+        System.out.println("Los dias mas calurosos fueron: ");
+        for (int i = 0; i < dia.length; i++) {
+            if (tempmax == dia[i].getTemperatura()) {
+                System.out.println("El " + dia[i].getNombreDia() + " dia " + i + ": "
+                        + "" + tempmax + " grados");
+            }
+        }
     }
 }
