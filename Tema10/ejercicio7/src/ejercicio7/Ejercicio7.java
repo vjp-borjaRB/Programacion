462e241d3e6f410a9da9d71368a931dca61f56cf
Neo <borr1316@gmail.com>
2026-02-27 17:46:36 +0100

    Update Ejercicio7.java

--- Tema10/ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Tema10/ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -11,7 +11,7 @@ public class Ejercicio7 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        ArrayList<ArrayList<Integer>> listaTemperaturas = new ArrayList<ArrayList<Integer>>();
+        ArrayList<ArrayList<Integer>> listaTemperaturas = new ArrayList<>();
         String[] diasSemana = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
         mainMenu(listaTemperaturas, diasSemana, entrada);
     }
@@ -26,32 +26,78 @@ public class Ejercicio7 {
 
     public static void mainMenu(ArrayList<ArrayList<Integer>> listaTemperaturas, String[] diasSemana, Scanner entrada) {
         int menu;
-        mostrarOpciones();
-        menu = pedirInt(entrada);
+        boolean salir;
         do {
+            salir = false;
+            mostrarOpciones();
+            menu = pedirInt(entrada);
             switch (menu) {
                 case 1 -> {
                     rellenarTemperaturas(listaTemperaturas, diasSemana, entrada);
                 }
                 case 2 -> {
+                    mostrarTemperaturas(listaTemperaturas, diasSemana);
                 }
                 case 3 -> {
+                    mostrarTempMediaMes(listaTemperaturas);
                 }
                 case 4 -> {
+                    mostrarMasCalurosos(listaTemperaturas, diasSemana);
                 }
                 case 5 -> {
+                    salir = true;
                 }
             }
-        } while (menu != 5);
+        } while (!salir);
     }
 
     public static void rellenarTemperaturas(ArrayList<ArrayList<Integer>> listaTemperaturas, String[] diasSemana, Scanner entrada) {
-        for (int i = 0; i <= 3; i++) {
-            listaTemperaturas.add(new ArrayList<Integer>());
+        for (int i = 0; i < 4; i++) {
+            int temperatura;
+            listaTemperaturas.add(new ArrayList());
+            System.out.println("Semana " + i);
+            for (int j = 0; j < 7; j++) {
+                System.out.println("Introduce la temperatura del dia " + diasSemana[j]);
+                temperatura = pedirInt(entrada);
+                listaTemperaturas.get(i).add(temperatura);
+            }
+        }
+    }
+
+    public static void mostrarTemperaturas(ArrayList<ArrayList<Integer>> listaTemperaturas, String[] diasSemana) {
+        for (int i = 0; i < listaTemperaturas.size(); i++) {
             System.out.println("Semana " + i);
             for (int j = 0; j < listaTemperaturas.get(i).size(); j++) {
-                System.out.println("Introduce la temperatura del dia " + diasSemana[j]);
-                listaTemperaturas.get(i).add(pedirInt(entrada));
+                System.out.println(diasSemana[j] + ": " + listaTemperaturas.get(i).get(j));
+            }
+        }
+    }
+
+    public static void mostrarTempMediaMes(ArrayList<ArrayList<Integer>> listaTemperaturas) {
+        int tempMedia = 0;
+        for (int i = 0; i < listaTemperaturas.size(); i++) {
+            for (int j = 0; j < listaTemperaturas.get(i).size(); j++) {
+                tempMedia += listaTemperaturas.get(i).get(j);
+            }
+        }
+        System.out.println("La temperatura media del mes es: " + tempMedia / 28);
+    }
+
+    public static void mostrarMasCalurosos(ArrayList<ArrayList<Integer>> listaTemperaturas, String[] diasSemana) {
+        int tempMax = Integer.MIN_VALUE;
+        for (int i = 0; i < listaTemperaturas.size(); i++) {
+            for (int j = 0; j < listaTemperaturas.get(i).size(); j++) {
+                int temperaturaDia = listaTemperaturas.get(i).get(j);
+                if (temperaturaDia > tempMax) {
+                    tempMax = temperaturaDia;
+                }
+            }
+        }
+        for (int y = 0; y < listaTemperaturas.size(); y++) {
+            for (int z = 0; z < listaTemperaturas.get(y).size(); z++) {
+                if (listaTemperaturas.get(y).get(z) == tempMax) {
+                    System.out.println("El dia " + diasSemana[z] + " de la semana " + y + " con " + listaTemperaturas.get(y).get(z) + " grados");
+                }
             }
         }
     }
