581ea13654cd11ca63d055139cfb6dbacd318d36
vjp-borjaRB <borr1316@gmail.com>
2026-04-09 17:49:23 +0200

    .

---
+++ Tema11/ejercicio8/src/ejercicio8/Ejercicio8.java
@@ -0,0 +1,15 @@
+package ejercicio8;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Borja Romero
+ */
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+        ArrayList<Ciudad> listaCiudades = new ArrayList();
+    }
+
+}
