34cb6c92bbc9ba81e32b77eb19fb3818625a27d9
Neo <borr1316@gmail.com>
2026-04-10 18:05:06 +0200

    EJ1

---
+++ Tema12/ejercicio1/src/ejercicio1/Contacto.java
@@ -0,0 +1,54 @@
+package ejercicio1;
+
+/**
+ *
+ * @author Borja Romero
+ */
+public class Contacto {
+
+    private String nombre;
+    private int edad;
+    private String telefono;
+
+    public Contacto() {
+        nombre = "";
+        edad = 0;
+        telefono = "";
+    }
+
+    public Contacto(String nombre, int edad, String telefono) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" + "nombre=" + nombre + ", edad=" + edad + ", telefono=" + telefono + '}';
+    }
+
+}
--- Tema12/ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Tema12/ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,5 +1,8 @@
 package ejercicio1;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 /**
  *
  * @author Borja Romero
@@ -7,7 +10,56 @@ package ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        
+        ArrayList<Contacto> listaContactos = new ArrayList<>();
     }
-    
+
+    public static void mainMenu() {
+        int menu;
+        do {
+            mostrarOpciones();
+            menu = pedirInt();
+            switch (menu) {
+                case 1 -> {
+                }
+                case 2 -> {
+                }
+                case 3 -> {
+                }
+                case 4 -> {
+                }
+            }
+        } while (menu != 5);
+    }
+
+    public static void addContactos() {
+        String nombre, telefono;
+        int edad;
+        System.out.print("Introduce el nombre: ");
+        nombre = pedirString();
+        System.out.print("Introduce la edad: ");
+        edad = pedirInt();
+        System.out.print("Introduce el telefono: ");
+        telefono = pedirString();
+    }
+
+    public static void mostrarOpciones() {
+        System.out.println("1. Add contactos");
+        System.out.println("2. Visualizar contactos");
+        System.out.println("3. Eliminar contactos de la lista");
+        System.out.println("4. Mostrar contactos por edad");
+        System.out.println("5. Salir del programa");
+    }
+
+    public static int pedirInt() {
+        Scanner entradaInt = new Scanner(System.in);
+        int entero = entradaInt.nextInt();
+        return entero;
+    }
+
+    public static String pedirString() {
+        Scanner entradaString = new Scanner(System.in);
+        String cadena = entradaString.nextLine();
+        return cadena;
+    }
+
 }
