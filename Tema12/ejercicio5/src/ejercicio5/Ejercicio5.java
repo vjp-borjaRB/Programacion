a3d7a5f79977732adfd829d9b927328cf013b3f4
vjp-borjaRB <borr1316@gmail.com>
2026-04-16 03:26:46 +0200

    ejercicio5

--- Tema12/ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Tema12/ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -2,63 +2,74 @@ package ejercicio5;
 
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 /**
  *
- * @author Borja Romero
+ * @author
  */
 public class Ejercicio5 {
 
     public static void main(String[] args) throws IOException {
-        FileWriter fwr = new FileWriter("agenda.txt");
-        BufferedWriter bwr = new BufferedWriter(fwr);
-        FileReader fr = new FileReader("agenda.txt");
-        BufferedReader br = new BufferedReader(fr);
-        mainMenu(fwr, bwr, fr, br);
+        mainMenu();
     }
 
-    public static void mainMenu(FileWriter fwr, BufferedWriter bwr, FileReader fr, BufferedReader br) {
+    public static void mainMenu() {
         int menu;
         do {
             mostrarOpciones();
             menu = pedirInt();
-            try {
-                switch (menu) {
-                    case 1 -> {
-                        addContactos(fwr, bwr);
-                    }
-                    case 2 -> {
-                        visualizarLista(fr, br);
-                    }
+            switch (menu) {
+                case 1 -> {
+                    addContactos();
                 }
-            } catch (InputMismatchException e) {
-                System.err.println("Introduce el tipo de dato correcto");
-            } catch (IOException ei) {
-                System.err.println("Error al escribir el archivo");
+                case 2 -> {
+                    visualizarLista();
+                }
             }
         } while (menu != 3);
     }
 
-    public static void addContactos(FileWriter fwr, BufferedWriter bwr) throws InputMismatchException, IOException {
-        String nombre, telefono;
-        int edad;
-        System.out.print("Introduce el nombre: ");
-        nombre = pedirString();
-        System.out.print("Introduce la edad: ");
-        edad = pedirInt();
-        System.out.print("Introduce el telefono: ");
-        telefono = pedirString();
-        Contacto nuevo = new Contacto(nombre, edad, telefono);
-        
+    public static void addContactos() {
+        try {
+            FileWriter fwr = new FileWriter("agenda.txt", true);
+            try (PrintWriter pwr = new PrintWriter(fwr)) {
+                String nombre, telefono;
+                int edad;
+                System.out.print("Introduce el nombre: ");
+                nombre = pedirString();
+                System.out.print("Introduce la edad: ");
+                edad = pedirInt();
+                System.out.print("Introduce el telefono: ");
+                telefono = pedirString();
+                Contacto nuevo = new Contacto(nombre, edad, telefono);
+                pwr.println(nuevo);
+            }
+        } catch (IOException e) {
+            System.err.println("No se encuentra el archivo");
+        } catch (InputMismatchException f) {
+            System.err.println("Introduce el tipo de dato correcto");
+        }
     }
 
-    public static void visualizarLista(FileReader fr, BufferedReader br) {
-
+    public static void visualizarLista() {
+        try {
+            FileReader fr = new FileReader("agenda.txt");
+            try (BufferedReader br = new BufferedReader(fr)) {
+                String linea;
+                linea = br.readLine();
+                while (linea != null) {
+                    System.out.println(linea);
+                    linea = br.readLine();
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer al archivo");
+        }
     }
 
     public static void mostrarOpciones() {
@@ -78,5 +89,4 @@ public class Ejercicio5 {
         String cadena = entradaString.nextLine();
         return cadena;
     }
-
 }
